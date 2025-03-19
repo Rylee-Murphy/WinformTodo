e3cc1b0b0ff2b385d1a41c120b7f49fab6ff8b2f
Rylee <Rylee.Murphy@student.centralia.edu>
2025-03-18 19:39:04 -0700

    Added missing stuff

--- WinformTodo/Form1.cs
+++ WinformTodo/Form1.cs
@@ -2,10 +2,75 @@ namespace WinformTodo
 {
     public partial class Form1 : Form
     {
+        private List<Todo> TaskList {  get; set; }
+
         public Form1()
         {
             InitializeComponent();
+            TaskList = new List<Todo>();
         }
+
+        private void SubmitForm(object sender, EventArgs e)
+        {
+            //
+            //
+            //
+            if (Validators.IsEmptyText(txtTaskDescription))
+            {
+                MessageBox.Show("Description is empty, please provide a description.");
+                return;
+            }
+
+            if (Validators.IsTextNull(txtTaskDescription))
+            {
+                MessageBox.Show("Description needs to be created.");
+                return;
+            }
+
+            if (Validators.IsEmptyText(txtDueDate))
+            {
+                MessageBox.Show("Missing a due date");
+                return;
+            }
+
+            if (!Validators.IsValidDate(txtDueDate))
+            {
+                MessageBox.Show("Date is incorrectly formatted, please resubmit.");
+            }
+
+
+
+
+            //
+            Todo myTodo = new Todo(txtTaskDescription.Text, DateTime.Parse(txtDueDate.Text));
+
+            //
+            TaskList.Add(myTodo);
+
+            UpdateListBox();
+
+            ClearForm();
+        }
+
+        public void UpdateListBox()
+        {
+            //
+            //
+
+            //
+            var list = TaskList
+            .OrderBy(t => t.DueDate)
+            .ToList();
+
+
+            //
+            for (int i = 0; i < list.Count; i++)
+            {
+            }
+
+            //
+        }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -17,30 +82,7 @@ namespace WinformTodo
             SubmitForm(sender, e);
             ClearForm();
         }
-        private void SubmitForm(object sender, EventArgs e)
-        {
-            if (Validators.IsEmptyText(txtTaskDescription))
-            {
-                MessageBox.Show("Description is empty, please provide a description.");
-                return;
-            }
-            if (Validators.IsTextNull(txtTaskDescription))
-            {
-                MessageBox.Show("Description needs to be created.");
-            }
-            if (Validators.IsEmptyText(txtDueDate))
-            {
-                MessageBox.Show("Missing a due date");
-                return;
-            }
-            if (!Validators.IsValidDate(txtDueDate))
-            {
-                MessageBox.Show("Date is incorrectly formatted, please resubmit.");
-            }
-            Todo myTodo = new Todo(txtTaskDescription.Text, DateTime.Parse(txtDueDate.Text));
-            lbTask.Items.Add(myTodo.ToString());
-            ClearForm();
-        }
+ 
         private void lbTask_SelectedIndexChanged(object sender, EventArgs e)
         {
         }
